56e785431d5a8a898283bd1f780c16321549d68b
mishal008 <mistye999@gmail.com>
2022-11-25 10:08:59 +0000

    changes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # restApiFull
+
+1. npm init -y
+2. npm install express
+3. npm install express mongoose dotenv
+4. connection string on .env
+5. setup db connection
+6. setup userModel db schema
---
+++ src/user/userController.js
@@ -0,0 +1,10 @@
+const User= require('./userModel')
+
+exports.createUser = async (request, response)=>{
+    try {
+        const
+         newUser = await User
+    } catch (error) {
+        
+    }
+}
