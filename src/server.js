1712ff04d3bce5d96f157e14ea7250b9d3338463
mishal008 <mistye999@gmail.com>
2022-12-01 11:35:14 +0000

    added procfile

--- src/server.js
+++ src/server.js
@@ -1,19 +1,23 @@
-require('./db/connection')
+require("./db/connection");
 
-const express = require("express")
-const userRouter = require('./user/userRouter')
+const express = require("express");
+const userRouter = require("./user/userRouter");
 
-const app = express()
+const app = express();
 
-const cors = require('cors')
-app.use(cors())
+const cors = require("cors");
+app.use(cors());
 
-const port = process.env.PORT || 5001
+const port = process.env.PORT || 5001;
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(userRouter)
+app.use(userRouter);
+
+app.get("/health", (req, res) => {
+  res.status(200).send({ message: "API is working" });
+});
 
 app.listen(port, () => {
-    console.log(`Listening to port ${port}`)
-})
+  console.log(`Listening to port ${port}`);
+});
